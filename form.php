3a770d625ffa515046d92e68a5cc63bea8e730df
Jannis Günsche <OfficialPandis@users.noreply.github.com>
2020-12-28 13:00:41 +0100

    Removed jQuery import

--- form.php
+++ form.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <title>HTML Upload Form</title>
 
-        <!-- jQuery import. Wird genutzt um über AJAX das Formular abzuschicken und anschließend die Antwort in die Tabelle zu schreiben-->
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-
     </head>
     <body>
 
